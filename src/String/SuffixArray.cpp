2eacda23e10fb343a3cc397d0701880a06fd4f5e
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-17 16:58:31 +0900

    Update SuffixArray.cpp

--- src/String/SuffixArray.cpp
+++ src/String/SuffixArray.cpp
@@ -1,35 +1,37 @@
-namespace Suffix {
-    static const int MX = 100010;
-    int RA[MX<<1], t[MX], C[MX];
-    
-    void build_SA(int N, char A[], int SA[], int LCP[]){
-        int cnt = 130;
-        for(int i=1;i<=N;i++)RA[i] = A[i];
-        for(int i=1;i<=N;i++)C[RA[i]]++;
-        for(int i=2;i<=cnt;i++)C[i] += C[i-1];
-        for(int i=1;i<=N;i++)SA[C[RA[i]]--] = i;
-        for(int i=1;i<=cnt;i++)C[i] = 0;
-        for(int L=1;;L<<=1){
-            int z = 0;
-            for(int i=N-L+1;i<=N;i++)t[++z] = i;
-            for(int i=1;i<=N;i++)if(SA[i] > L)t[++z] = SA[i] - L;
-            for(int i=1;i<=N;i++)C[RA[i]]++;
-            for(int i=2;i<=cnt;i++)C[i] += C[i-1];
-            for(int i=N;i;i--)SA[ C[RA[t[i]]]-- ] = t[i];
-            for(int i=1;i<=cnt;i++)C[i] = 0;
-            cnt = 1;
-            for(int i=1;i<=N;i++){
-                if(i != 1 && RA[SA[i]] == RA[SA[i-1]] && RA[SA[i] + L] == RA[SA[i-1] + L])C[SA[i]] = cnt-1;
-                else C[SA[i]] = cnt++;
-            }
-            for(int i=1;i<=N;i++)RA[i] = C[i], C[i] = 0;
-            if(cnt == N+1)break;
-        }
-        for(int i=1, L=0;i<=N;i++, L=(L?L-1:0)){
-            if(RA[i] == N)continue;
-            int t = SA[RA[i]+1];
-            while(A[i+L] == A[t+L])++L;
-            LCP[RA[i]] = L;
+const int MAXN = 1e5 + 50;
+int cnt[MAXN], x[MAXN], y[MAXN];
+void suffixArray(int n, char *s, int *sa, int *rk) {
+    memset(cnt, 0, sizeof cnt);
+    int m = 255;
+    for(int i = 0; i < n; i++) cnt[x[i] = s[i]]++;
+    for(int i = 1; i <= m; i++) cnt[i] += cnt[i-1];
+    for(int i = 0; i < n; i++) sa[--cnt[x[i]]] = i;
+    for(int len = 1, sz = 1; sz < n; len <<= 1, m = sz) {
+        int p = 0;
+        for(int i = n-len; i < n; i++) y[p++] = i;
+        for(int i = 0; i < n; i++) if(sa[i] >= len) y[p++] = sa[i]-len;
+        for(int i = 1; i <= m; i++) cnt[i] = 0;
+        for(int i = 0; i < n; i++) cnt[x[i]]++;
+        for(int i = 1; i <= m; i++) cnt[i] += cnt[i-1];
+        for(int i = n-1; i >= 0; i--) sa[--cnt[x[y[i]]]] = y[i];
+        swap(x, y), sz = 1, x[sa[0]] = 1;
+        for(int i = 0; i < n-1; i++) {
+            if(y[sa[i]] != y[sa[i+1]] || y[sa[i]+len] != y[sa[i+1]+len]) sz++;
+            x[sa[i+1]] = sz;
         }
     }
-};
+
+    for(int i = 0; i < n; i++) rk[sa[i]] = i;
+}
+
+void findLcp(int n, char *s, int *sa, int *rk, int *lcp) {
+    int h = 0;
+    for(int i = 0; i < n; i++) {
+        if(rk[i]) {
+            int prv = sa[rk[i]-1];
+            while(s[prv+h] == s[i+h]) h++;
+            lcp[rk[i]] = h;
+        }
+        h = max(h-1, 0);
+    }
+}
