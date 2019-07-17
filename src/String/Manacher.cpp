0653897158ad52b0904b52d41143b7573a8442d0
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-17 17:00:27 +0900

    Update Manacher.cpp

--- src/String/Manacher.cpp
+++ src/String/Manacher.cpp
@@ -1,29 +1,21 @@
-// finds radius of longest palindrome centered at s[i]
-// If you also want to find even-length paindromes, use dummy characters
-// baab -> #b#a#a#b#
-vector<int> ManacherAlgorithm(string s){
-    int n = (int) s.size();
-    int p = -1, r = -1;
-    vector<int> A(n);
-    for(int i=0;i<n;i++){
+const int MAXN = 1e5 + 50;
+int A[MAXN], B[MAXN];
 
-        if( r < i ){
-            A[i] = 0;
-            int j = 0;
-            while( i + A[i] < n && i - A[i] >= 0 && s[ i+A[i] ] == s[ i-A[i] ] ) A[i]++;
-            A[i]--;
-        }
-        else{
-            A[i] = min( A[2*p - i] , r-i );
-            while( i + A[i] < n && i - A[i] >= 0 && s[ i+A[i] ] == s[ i-A[i] ] ) A[i]++;
-            A[i]--;
-        }
-
-        // update r
-        if( r < i + A[i] ){
-            r = i + A[i];
-            p = i;
-        }
+void manacher(string &s) {
+    int n = (int)s.size();
+    int R = -1, P = -1;
+    for(int i = 0; i < n; i++) {
+        if(i <= R) A[i] = min(A[2*P-i], R-i);
+        else A[i] = 0;
+        while(0 <= i-A[i]-1 && i+A[i]+1 < n && s[i-A[i]-1] == s[i+A[i]+1]) A[i]++;
+        if(R < i+A[i]) R = i+A[i], P = i;
     }
-    return A;
+    
+    int R = -1, P = -1;
+    for(int i = 0; i < n; i++) {
+        if(i <= R) B[i] = min(B[2*P-i], R-i+1);
+        else B[i] = 0;
+        while(0 <= i-B[i]-1 && i+B[i] < n && s[i-B[i]-1] == s[i+B[i]]) B[i]++;
+        if(R < i+B[i]-1) R = i+B[i]-1, P = i;
+    }
 }
