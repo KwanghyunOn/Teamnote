9c3c0953696acbf3fe79109606aeec6c027f341f
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-17 17:01:10 +0900

    Update KMP.cpp

--- src/String/KMP.cpp
+++ src/String/KMP.cpp
@@ -1,31 +1,23 @@
-vector<int> preprocess(string p){
-    int m = p.size();
-    vector<int> fail(m);
-    fail[0] = 0; int j = 0;
-    for(int i=1;i<m;i++){
-        while(j>0&&p[i]!=p[j]) j = fail[j-1];
-        if( p[i] == p[j] ){
-            fail[i] = j+1; j++;
-        }else{
-            fail[i] = 0;
-        }
-    }
-    return fail;
+int fail[MAXN];
+void findFailure(const string &P) {
+	fail[1] = 0;
+	int k = 0;
+	for(int i = 1; i < (int)P.size(); i++) {
+		while(k && P[i] != P[k]) k = fail[k];
+		if(P[i] == P[k]) k++;
+		fail[i+1] = k;
+	}
 }
 
-vector<int> kmp(string s, string p){
-    auto fail = preprocess(p);
-    vector<int> ans; int n = s.size(), m = p.size();
-    int j = 0;
-    for(int i=0;i<n;i++){
-        while(j>0 && s[i]!=p[j]) j = fail[j-1];
-        if( s[i] == p[j] ){
-            if( j == m-1 ){
-                ans.pb(i-m+1); j = fail[j];
-            }else{
-                j++;
-            }
-        }
-    }
-    return ans;
+// pattern P, string S
+void kmp(const string &P, const string &S, vector<int> &matchIndex) {
+	int k = 0;
+	for(int i = 0; i < (int)S.size(); i++) {
+		while(k && S[i] != P[k]) k = fail[k];
+		if(S[i] == P[k]) k++;
+		if(k == (int)P.size()) {
+			matchIndex.push_back(i-(int)P.size()+1);
+			k = fail[k];
+		}
+	}
 }
